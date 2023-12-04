b61a8e3dd38199a20026305a4f1b63fb6bac7835
Niyati sheth <niyati.radicalloop@gmail.com>
2023-12-04 10:32:57 +0530

    react bootstramp temp 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Services from "./components/Services";
+import About from "./components/About";
+import Clients from "./components/Clients";
+import Portfolio from "./components/Portfolio";
+import Context from "./components/Context";
+import Team from "./components/Team";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Services />
+      <Portfolio />
+      <About />
+      <Clients />
+      <Context />
+      <Team />
+      <Footer />
     </div>
   );
 }
